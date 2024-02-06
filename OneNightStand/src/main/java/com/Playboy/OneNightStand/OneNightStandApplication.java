c7b4e661bfceae14c25c5b731570cef8c90d5d52
B Pavan kumar <62787933+pavankumar468@users.noreply.github.com>
2024-02-06 19:44:30 +0530

    Add files via upload

---
+++ OneNightStand/src/main/java/com/Playboy/OneNightStand/OneNightStandApplication.java
@@ -0,0 +1,13 @@
+package com.Playboy.OneNightStand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneNightStandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneNightStandApplication.class, args);
+	}
+
+}
---
+++ OneNightStand/src/test/java/com/Playboy/OneNightStand/OneNightStandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Playboy.OneNightStand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneNightStandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
